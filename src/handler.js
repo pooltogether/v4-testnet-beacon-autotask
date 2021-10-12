d43b545211ea9b4fcf88706dbaed27ebcfc70927
Kames Geraghty <kamescg@gmail.com>
2021-10-12 15:00:25 -0400

    feat(Handler): migrate to v4-autotask-lib

--- src/handler.js
+++ src/handler.js
@@ -1,57 +1,18 @@
 const { Relayer } = require('defender-relay-client');
 const { getContracts } = require('./getContracts')
+const { drawBeaconHandleDrawStartAndComplete } = require('@pooltogether/v4-autotask-lib')
 
 async function handler(event) {
-  const rinkebyRelayer = new Relayer(event);
-  const {
-    infuraApiKey
-  } = event.secrets;
-  
-  const {
-    drawBeacon,
-  } = getContracts(infuraApiKey)
-
-  const nextDrawId = await drawBeacon.getNextDrawId()
-  const beaconPeriodStartedAt = await drawBeacon.getBeaconPeriodStartedAt()
-  const isBeaconPeriodOver = await drawBeacon.isRngRequested()
-  const beaconPeriodSeconds = await drawBeacon.getBeaconPeriodSeconds()
-
-  console.log('DrawBeacon Beacon PeriodStartedAt:', beaconPeriodStartedAt.toString())
-  console.log('DrawBeacon Beacon PeriodSeconds:', beaconPeriodSeconds.toString())
-  console.log('DrawBeacon Beacon PeriodOver:', isBeaconPeriodOver)
-  
-  console.log('DrawBeacon next Draw.drawId:', nextDrawId)
-
-  console.log('Is RNG Requested:', await drawBeacon.isRngRequested())
-  console.log('Can Start Draw:', await drawBeacon.canStartDraw())
-  console.log('Can Complete Draw:', await drawBeacon.canCompleteDraw())
-
-  if (await drawBeacon.canStartDraw()) {
-    console.log(`Starting draw ${nextDrawId}...`)
-    const tx = await drawBeacon.populateTransaction.startDraw()
-    const txRes = await rinkebyRelayer.sendTransaction({
-      data: tx.data,
-      to: tx.to,
-      speed: 'fast',
-      gasLimit: 500000,
-    });
-    console.log(`Started Draw ${nextDrawId}: ${txRes.hash}`)
+  const { infuraApiKey } = event.secrets;
+  const config = {
+    network: 'rinkeby',
+    chainId: 4,
+    apiKey: infuraApiKey,
+    speed: 'fast',
+    gasLimit: 50000
   }
-
-  let completedDraw = false
-  if (await drawBeacon.canCompleteDraw()) {
-    console.log(`Completing draw ${nextDrawId}...`)
-    const tx = await drawBeacon.populateTransaction.completeDraw()
-    const txRes = await rinkebyRelayer.sendTransaction({
-      data: tx.data,
-      to: tx.to,
-      speed: 'fast',
-      gasLimit: 500000,
-    });
-    console.log(`Completed Draw ${nextDrawId}: ${txRes.hash}`)
-    completedDraw = true
-  }
-  console.log("Handler Complete!")
+  const { msg, err, data } = await drawBeaconHandleDrawStartAndComplete(config, new Relayer(event))
+  console.log(msg)
 }
 
 exports.handler = handler
