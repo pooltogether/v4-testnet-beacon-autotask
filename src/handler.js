aae789e332fdd7cfcfb195ccf493cd331e96916f
Kames Geraghty <kamescg@gmail.com>
2021-10-12 15:43:15 -0400

    feat(Handler): integraate @pooltogether/v4-autotask-lib without execute process

--- src/handler.js
+++ src/handler.js
@@ -1,9 +1,10 @@
 const { Relayer } = require('defender-relay-client');
-const { getContracts } = require('./getContracts')
-const { drawBeaconHandleDrawStartAndComplete } = require('@pooltogether/v4-autotask-lib')
+const { drawBeaconHandleDrawStartAndComplete, utils } = require('@pooltogether/v4-autotask-lib')
 
 async function handler(event) {
   const { infuraApiKey } = event.secrets;
+  const relayer = new Relayer(event);
+  const provider = utils.getInfuraProvider('rinkeby', infuraApiKey)
   const config = {
     network: 'rinkeby',
     chainId: 4,
@@ -11,8 +12,24 @@ async function handler(event) {
     speed: 'fast',
     gasLimit: 50000
   }
-  const { msg, err, data } = await drawBeaconHandleDrawStartAndComplete(config, new Relayer(event))
-  console.log(msg)
+
+  // READ DrawBeacon State
+  const { msg, err, transaction, status } = await drawBeaconHandleDrawStartAndComplete(config, relayer)
+  if(err) return console.log(msg);
+
+  // IF DrawBeacon needs to pust a Draw state forward execute a transaction. 
+  if(status == 1) {
+    console.log('Executing:', msg)
+    ts = await relayer.sendTransaction({
+      data: transaction.data,
+      to: transaction.to,
+      speed: config.speed,
+      gasLimit: config.gasLimit,
+    });
+    const txRes = provider.getTransaction(ts.hash)
+    txRes.wait(1)
+    console.log('Completed:', msg)
+  }
 }
 
 exports.handler = handler
